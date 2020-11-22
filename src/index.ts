45e6ff1d474fd00f8674ffe4c7d6c5cf8b8bcb94
SukkaW <isukkaw@gmail.com>
2020-11-22 23:26:50 +0800

    feat: implement the package

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import { join } from 'path';
+
+interface ListDirOptions {
+  ignoreHidden?: boolean
+  ignorePattern?: RegExp | null
+}
+
+function listDir(path: string, options?: ListDirOptions): Promise<string[]> {
+  const results: string[] = [];
+
+  options = {
+    ignoreHidden: true,
+    ignorePattern: null,
+    ...options
+  };
+
+  return listDirWalker(path, results, '', options).then(() => results);
+}
+
+function listDirWalker(path: string, results: string[], parent: string, options: ListDirOptions): Promise<any[]> {
+  const promises: Promise<any[]>[] = [];
+
+  return readAndFilterDir(path, options).then(items => {
+    items.forEach(item => {
+      const currentPath = join(parent, item.name);
+
+      if (item.isDirectory()) {
+        promises.push(listDirWalker(join(path, item.name), results, currentPath, options));
+      } else {
+        results.push(currentPath);
+      }
+    });
+  }).then(() => Promise.all(promises));
+}
+
+function readAndFilterDir(path: string, options: ListDirOptions) {
+  const { ignoreHidden = true, ignorePattern } = options;
+
+  return fs.promises.readdir(path, { ...options, withFileTypes: true })
+    .then(results => {
+      if (ignoreHidden) {
+        results = results.filter(({ name }) => !name.startsWith('.'));
+      }
+      if (ignorePattern) {
+        results = results.filter(({ name }) => !ignorePattern.test(name));
+      }
+
+      return results;
+    });
+}
+
+export = listDir;
---
+++ test/index.ts
@@ -0,0 +1,80 @@
+import 'ts-mocha';
+import chai from 'chai';
+import { writeFileSync, mkdirSync, rmSync } from 'fs';
+import { join } from 'path';
+
+import listDir from '../src';
+
+chai.should();
+
+function createDummyFolder(path: string): void {
+  const filesMap = {
+    // Normal files in a hidden folder
+    [join('.hidden', 'a.txt')]: 'a',
+    [join('.hidden', 'b.js')]: 'b',
+    // Normal folder in a hidden folder
+    [join('.hidden', 'c', 'd')]: 'd',
+    // Top-class files
+    'e.txt': 'e',
+    'f.js': 'f',
+    // A hidden file
+    '.g': 'g',
+    // Files in a normal folder
+    [join('folder', 'h.txt')]: 'h',
+    [join('folder', 'i.js')]: 'i',
+    // A hidden files in a normal folder
+    [join('folder', '.j')]: 'j'
+  };
+
+  Object.entries(filesMap).map(([fileKey, fileContent]) => writeFileSync(join(path, fileKey), fileContent));
+}
+
+describe('@sukka/listdir', () => {
+  const tmpDir = join(__dirname, 'fs_tmp');
+  const target = join(tmpDir, 'test');
+
+  before(() => {
+    mkdirSync(join(target, 'folder'), { recursive: true });
+    mkdirSync(join(target, '.hidden', 'c'), { recursive: true });
+    createDummyFolder(target);
+  });
+  after(() => rmSync(tmpDir, { recursive: true, force: true }));
+
+  it('list dir', async () => {
+    const expected = [
+      'e.txt',
+      'f.js',
+      join('folder', 'h.txt'),
+      join('folder', 'i.js')
+    ];
+
+    const dir = await listDir(target);
+    dir.should.eql(expected);
+
+    // await fs.rmdir(target);
+  });
+
+  it('listDir() - ignoreHidden off', async () => {
+    const filenames = [
+      join('.hidden', 'a.txt'),
+      join('.hidden', 'b.js'),
+      join('.hidden', 'c', 'd'),
+      'e.txt',
+      'f.js',
+      '.g',
+      join('folder', 'h.txt'),
+      join('folder', 'i.js'),
+      join('folder', '.j')
+    ];
+
+    const dir = await listDir(target, { ignoreHidden: false });
+    dir.should.have.members(filenames);
+  });
+
+  it('listDir() - ignorePattern', async () => {
+    const target = join(tmpDir, 'test');
+
+    const dir = await listDir(target, { ignorePattern: /\.js/ });
+    dir.should.eql(['e.txt', join('folder', 'h.txt')]);
+  });
+});
